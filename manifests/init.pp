9e3500446a37286dc98b9787b06b7f604a03cf7a
Federico Voges <federico@puppet.com>
2019-07-01 10:29:04 +0100

    reformat for readability

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,21 +12,21 @@
 #
 class pe_backup (
   Stdlib::Absolutepath $destination,
-  String $user                       = $::pe_backup::params::user,
-  String $group                      = $::pe_backup::params::group,
-  String $umask                      = $::pe_backup::params::umask,
-  String $prefix                     = $::pe_backup::params::prefix,
-  Stdlib::Absolutepath $script       = $::pe_backup::params::script,
-  String $hour                       = $::pe_backup::params::hour,
-  String $minute                     = $::pe_backup::params::minute,
-  String $weekday                    = $::pe_backup::params::weekday,
-  Array  $dirs_common                = $::pe_backup::params::dirs_common,
-  Array  $dirs_master                = $::pe_backup::params::dirs_master,
-  Array  $dirs_db                    = $::pe_backup::params::dirs_db,
-  Array  $dirs_console               = $::pe_backup::params::dirs_console,
-  Array  $dirs_extra                 = $::pe_backup::params::dirs_extra,
-  Enum['monolithic','master','db','console'] $nodetype = $::pe_backup::params::nodetype,
-) inherits ::pe_backup::params {
+  String                                     $user          = $::pe_backup::params::user,
+  String                                     $group         = $::pe_backup::params::group,
+  String                                     $umask         = $::pe_backup::params::umask,
+  String                                     $prefix        = $::pe_backup::params::prefix,
+  Stdlib::Absolutepath                       $script        = $::pe_backup::params::script,
+  String                                     $hour          = $::pe_backup::params::hour,
+  String                                     $minute        = $::pe_backup::params::minute,
+  String                                     $weekday       = $::pe_backup::params::weekday,
+  Array                                      $dirs_common   = $::pe_backup::params::dirs_common,
+  Array                                      $dirs_master   = $::pe_backup::params::dirs_master,
+  Array                                      $dirs_db       = $::pe_backup::params::dirs_db,
+  Array                                      $dirs_console  = $::pe_backup::params::dirs_console,
+  Array                                      $dirs_extra    = $::pe_backup::params::dirs_extra,
+  Enum['monolithic','master','db','console'] $nodetype      = $::pe_backup::params::nodetype,
+) inherits pe_backup::params {
 
   case $nodetype {
     'monolithic': {
@@ -47,8 +47,8 @@ class pe_backup (
     }
   }
 
-  contain ::pe_backup::install
-  contain ::pe_backup::service
+  contain pe_backup::install
+  contain pe_backup::service
 
-  Class['::pe_backup::install'] -> Class['::pe_backup::service']
+  Class['pe_backup::install'] -> Class['pe_backup::service']
 }
